922ba889e7e3c4c61f1dfe2e14b4f6806ff87369
Ritwik <gritwik5843@gmail.com>
2025-01-20 21:33:48 +0530

    fix typo

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,6 +1,6 @@
 const homepage=(req,res)=>{
     try {
-        res.status(200).send("Hlw from Home Page")
+        res.status(200).send("Hellow from Home Page")
     } catch (error) {
         res.status(401).send("Error to fetch the home page")
     }
