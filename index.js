321556ca3f8cb662614a7b0b6fbfa2782713f91b
Ritwik <gritwik5843@gmail.com>
2025-01-20 20:16:36 +0530

    add cors

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ require("dotenv").config();
 const express=require("express");
 const contactform = require("./router/contact");
 const dbconnect=require("./Database/db");
+const cors=require('cors');
 const homepage = require("./router/homepage-router");
 const app=express();
+
+
+const coreoption={
+    origin:"http://localhost:3000",
+    methods:"GET,POST,PATCH,PUT,DELETE",
+    credentials:true
+  }
+
+  //use cors 
+app.use(cors(coreoption))
 
 //necessary middleware in place to parse incoming JSON data
 app.use(express.json());
